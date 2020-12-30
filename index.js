42311e1837e867583eaf4fe2f0e684d3eaf41fb1
Rolland Orgonas <rorgonas@gmail.com>
2020-12-30 13:11:11 -0500

    quasar - setup - server

---
+++ index.js
@@ -0,0 +1,34 @@
+/**
+ * Dependencies
+ * */
+
+  const express = require('express')
+
+/**
+ * Config
+ * */
+
+  const app = express()
+  const port = 3000
+
+
+/**
+ * Endpoints
+ * */
+  app.get('/posts', (req, res) => {
+    let posts = [
+      {
+        caption: 'sdfsdf',
+        location: 'fff'
+      },
+      {
+        caption: '234234',
+        location: '234'
+      },
+    ]
+    res.send(posts)
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
